e518a43752b247cf3ec9f32af6bb05f9608ab24a
Mingjie Li <mingjiecn@gmail.com>
2023-08-23 11:22:09 -0500

    add fetch request

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,41 @@
 import PropTypes from "prop-types";
-import { API_URL } from "../lib/constants";
-import { getQueryStringFromServerQuery } from "../lib/query-utils";
 import Breadcrumbs from "../components/breadcrumbs";
-import JsonDisplay from "../components/json-display";
+import {
+  DataArea,
+  DataItemLabel,
+  DataItemValue,
+  DataPanel,
+} from "../components/data-area";
+import Notifications from "../components/notifications";
 import PagePreamble from "../components/page-preamble";
+import RegulomeVersionTag from "../components/regulome-version-tag";
+import errorObjectToProps from "../lib/errors";
+import FetchRequest from "../lib/fetch-request";
+import { getQueryStringFromServerQuery } from "../lib/query-utils";
 
 export default function Search({ data }) {
   return (
     <>
       <Breadcrumbs />
+      <RegulomeVersionTag />
       <PagePreamble />
-      <JsonDisplay item={data} />
+      {data.total === 1 && (
+        <DataPanel>
+          <DataArea>
+            <DataItemLabel>Searched Coordinates</DataItemLabel>
+            <DataItemValue>{data.query_coordinates[0]}</DataItemValue>
+            <DataItemLabel>Genome Assembly</DataItemLabel>
+            <DataItemValue>{data.assembly}</DataItemValue>
+            <DataItemLabel>Rank</DataItemLabel>
+            <DataItemValue>{data.regulome_score.ranking}</DataItemValue>
+            <DataItemLabel>Score</DataItemLabel>
+            <DataItemValue>{data.regulome_score.probability}</DataItemValue>
+          </DataArea>
+        </DataPanel>
+      )}
+      {Object.keys(data.notifications).length > 0 && (
+        <Notifications notifications={data.notifications} />
+      )}
     </>
   );
 }
@@ -21,21 +46,27 @@ Search.propTypes = {
 
 export async function getServerSideProps({ query }) {
   const queryString = getQueryStringFromServerQuery(query);
-  const url = `${API_URL}/search?${queryString}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const breadcrumbs = [
-    {
-      title: "Search",
-      href: `/search?${queryString}`,
-    },
-  ];
-
-  return {
-    props: {
-      data,
-      breadcrumbs,
-      pageContext: { title: "Search" },
-    },
-  };
+  const request = new FetchRequest();
+  const data = await request.getObject(`/search?${queryString}`);
+  if (FetchRequest.isResponseSuccess(data)) {
+    const breadcrumbs = [
+      {
+        title: "Search",
+        href: `/search?${queryString}`,
+      },
+    ];
+    return {
+      props: {
+        data,
+        breadcrumbs,
+        pageContext: {
+          title:
+            data.query_coordinates.length < 1
+              ? "Search"
+              : data.query_coordinates[0],
+        },
+      },
+    };
+  }
+  return errorObjectToProps(data);
 }
