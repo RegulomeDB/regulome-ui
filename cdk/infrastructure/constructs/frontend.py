7eec73ce36610884971a2b6e7782e9ac2d7c82c6
Otto Jolanki <ojolanki@stanford.edu>
2023-08-28 12:33:49 -0700

    health check /search endpoint

--- cdk/infrastructure/constructs/frontend.py
+++ cdk/infrastructure/constructs/frontend.py
@@ -127,7 +127,8 @@ class Frontend(Construct):
     def _configure_health_check(self) -> None:
         self.fargate_service.target_group.configure_health_check(
             interval=Duration.seconds(60),
-            healthy_http_codes='200,308',
+            healthy_http_codes='200',
+            path='/search',
         )
 
     def _add_tags_to_fargate_service(self) -> None:
