7f2ede7a13001deb629b4e9dd9771ca27a05d31f
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2023-08-29 16:35:17 -0500

    REGD-119-search (#7)

---
+++ components/snps-diagram.js
@@ -0,0 +1,176 @@
+import PropTypes from "prop-types";
+import { DataAreaTitle, DataPanel } from "./data-area";
+
+const MIN_DISTANCE_BETWEEN_LABELS = 90;
+const TEXT_VERTICAL_DISTANCE = 20;
+const LABEL_HEIGHT = 18;
+const CHAR_SIZE = 8.5;
+const DISTANCE_BETWEEN_FIRST_AND_LAST_SNP = 920;
+const LINE_STARTING_X = 10;
+const LINE_ENDING_X = 990;
+const LINE_Y = 75;
+const TICK_STARTING_Y_EVEN = 60;
+const TICK_STARTING_Y_ODD = 90;
+const TEXT_STARTING_Y_EVEN = 55;
+const TEXT_STARTING_Y_ODD = 105;
+
+/**
+ * Display SNPs Matching Searched Coordinates and Nearby SNPs using line chart
+ */
+export default function SnpsDiagram({ data }) {
+  const startSnpCoordinate = data.nearby_snps[0].coordinates.lt;
+  const endSnpCoordinate =
+    data.nearby_snps[data.nearby_snps.length - 1].coordinates.lt;
+  const hitSnpX = getX(
+    data.query_coordinates[0].split("-")[1],
+    startSnpCoordinate,
+    endSnpCoordinate
+  );
+  const chartElements = [];
+  const yOffsets = [];
+  let numberSign = 1;
+  data.nearby_snps.map((snp, snpIndex) => {
+    const isEven = snpIndex % 2 === 0;
+    const snpX = getX(snp.coordinates.lt, startSnpCoordinate, endSnpCoordinate);
+    const isHitSnp = snpX === hitSnpX;
+    const labelWidth = snp.rsid.length * CHAR_SIZE;
+    const textX = snpX - labelWidth / 2;
+    let yOffset = 0;
+    const xYOffset = [snpX];
+    yOffsets.push(xYOffset);
+
+    if (snpX - yOffsets[snpIndex - 2]?.[0] < MIN_DISTANCE_BETWEEN_LABELS) {
+      yOffset = yOffsets[snpIndex - 2][1]
+        ? yOffsets[snpIndex - 2][1] + TEXT_VERTICAL_DISTANCE * numberSign
+        : TEXT_VERTICAL_DISTANCE * numberSign;
+    }
+    numberSign = numberSign * -1;
+
+    xYOffset.push(yOffset);
+    const lineChartElement = {
+      line: {
+        x1: String(snpX),
+        x2: String(snpX),
+        y1: isEven
+          ? TICK_STARTING_Y_EVEN - yOffsets[snpIndex][1]
+          : TICK_STARTING_Y_ODD - yOffsets[snpIndex][1],
+        stroke: isHitSnp ? "#c13b42" : "#7F7F7F",
+      },
+      rect: {
+        x: textX - 2,
+        y: isEven
+          ? TICK_STARTING_Y_EVEN - LABEL_HEIGHT - yOffsets[snpIndex][1]
+          : TICK_STARTING_Y_ODD - yOffsets[snpIndex][1],
+        width: labelWidth,
+        fill: isHitSnp ? "#c13b42" : "white",
+        opacity: isHitSnp ? "1.0" : "0.6",
+      },
+      text: {
+        x: textX,
+        y: isEven
+          ? TEXT_STARTING_Y_EVEN - yOffsets[snpIndex][1]
+          : TEXT_STARTING_Y_ODD - yOffsets[snpIndex][1],
+        className: isHitSnp ? "fill-white" : null,
+        rsid: snp.rsid,
+      },
+    };
+    chartElements.push(lineChartElement);
+  });
+  chartElements.sort((a, b) => {
+    return Math.abs(a.line.y1 - b.line.y1) * -1;
+  });
+
+  return (
+    <>
+      <DataAreaTitle>
+        SNPs Matching Searched Coordinates and Nearby SNPs
+      </DataAreaTitle>
+      <DataPanel>
+        <div className="flex justify-center mb-3 font-semibold">
+          Chromosome {data.nearby_snps[0].chrom.split("chr")[1]}
+        </div>
+        <svg
+          viewBox="-10 -30 1050 220"
+          preserveAspectRatio="xMidYMid meet"
+          aria-labelledby="diagram-of-nearby-snps"
+          role="img"
+        >
+          <title id="diagram-of-nearby-snps">Diagram of nearby SNPs</title>
+          <defs>
+            <marker
+              id="arrow"
+              viewBox="0 0 10 10"
+              refX="5"
+              refY="5"
+              markerWidth="5"
+              markerHeight="6"
+              orient="auto-start-reverse"
+            >
+              <path d="M 0 0 L 10 5 L 0 10 z" />
+            </marker>
+          </defs>
+          <g id="x-axis">
+            <line
+              x1={LINE_STARTING_X}
+              x2={LINE_ENDING_X}
+              y1={LINE_Y}
+              y2={LINE_Y}
+              markerEnd="url(#arrow)"
+              markerStart="url(#arrow)"
+              stroke="#7F7F7F"
+              strokeWidth="2"
+            />
+          </g>
+          <g className="text-sm">
+            {chartElements.map((item) => {
+              return (
+                <g key={item.text.rsid}>
+                  <line
+                    id="tick"
+                    x1={item.line.x1}
+                    x2={item.line.x2}
+                    y1={item.line.y1}
+                    y2={LINE_Y}
+                    stroke={item.line.stroke}
+                    strokeWidth="2"
+                  />
+                  <rect
+                    id="lable-background-color"
+                    x={item.rect.x}
+                    y={item.rect.y}
+                    height={LABEL_HEIGHT}
+                    width={item.rect.width}
+                    fill={item.rect.fill}
+                    opacity={item.rect.opacity}
+                    rx="2px"
+                  />
+                  <text
+                    id="lable-text"
+                    x={item.text.x}
+                    y={item.text.y}
+                    className={item.text.className}
+                  >
+                    {item.text.rsid}
+                  </text>
+                </g>
+              );
+            })}
+          </g>
+        </svg>
+      </DataPanel>
+    </>
+  );
+}
+
+SnpsDiagram.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+function getX(snpCoordinate, startSnpCoordinate, endSnpCoordinate) {
+  return (
+    DISTANCE_BETWEEN_FIRST_AND_LAST_SNP *
+      ((snpCoordinate - startSnpCoordinate) /
+        (endSnpCoordinate - startSnpCoordinate)) +
+    MIN_DISTANCE_BETWEEN_LABELS / 2
+  );
+}
--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import React, { useState } from "react";
+import _ from "lodash";
 import Breadcrumbs from "../components/breadcrumbs";
 import {
   DataArea,
@@ -6,36 +8,177 @@ import {
   DataItemValue,
   DataPanel,
 } from "../components/data-area";
+import { Button } from "../components/form-elements";
 import Notifications from "../components/notifications";
 import PagePreamble from "../components/page-preamble";
 import RegulomeVersionTag from "../components/regulome-version-tag";
+import SnpsDiagram from "../components/snps-diagram";
 import errorObjectToProps from "../lib/errors";
 import FetchRequest from "../lib/fetch-request";
+import filterOverlappingPeaks from "../lib/filter-overlapping-peaks";
+import getSnpsInfo from "../lib/get-snps-info";
 import { getQueryStringFromServerQuery } from "../lib/query-utils";
 
+// Default number of populations to display for allele frequencies.
+const defaultDisplayCount = 3;
+
 export default function Search({ data }) {
-  return (
-    <>
-      <Breadcrumbs />
-      <RegulomeVersionTag />
-      <PagePreamble />
-      {data.total === 1 && (
+  const [showMoreFreqs, setShowMoreFreqs] = useState(false);
+  if (Object.keys(data.notifications).length === 0) {
+    const hitSnps = getSnpsInfo(data);
+    const coordinates = data.query_coordinates[0];
+    const allData = data["@graph"] || [];
+    const QTLData = allData.filter(
+      (d) => d.method && d.method.indexOf("QTL") !== -1
+    );
+
+    const chromatinData = allData.filter((d) => d.method === "chromatin state");
+    const chipData = filterOverlappingPeaks(
+      allData.filter((d) => d.method === "ChIP-seq")
+    );
+    const dnaseData = filterOverlappingPeaks(
+      allData.filter(
+        (d) =>
+          d.method === "FAIRE-seq" ||
+          d.method === "DNase-seq" ||
+          d.method === "ATAC-seq"
+      )
+    );
+    const histoneData = filterOverlappingPeaks(
+      allData.filter((d) => d.method === "Histone ChIP-seq")
+    );
+
+    const motifsData = filterOverlappingPeaks(
+      allData.filter((d) => d.method === "footprints" || d.method === "PWMs")
+    );
+
+    const duplicatedExperimentDatasets = data["@graph"].filter((d) =>
+      d.dataset.includes("experiment")
+    );
+    // for some reason we are getting duplicates here so we need to filter those out
+    const experimentDatasets = _.uniqBy(
+      duplicatedExperimentDatasets,
+      "dataset"
+    );
+    experimentDatasets.sort((a, b) => (a.method > b.method ? 1 : -1));
+    // genome browser files
+    let filesForGenomeBrowser = [];
+    experimentDatasets.forEach((dataset) => {
+      const files = dataset.files_for_genome_browser;
+      let target = "";
+      // use target labels instead of gene symbols for histone ChIP-seq targets
+      if (dataset.method === "Histone ChIP-seq") {
+        target = dataset.target_label ? dataset.target_label : "";
+      } else {
+        target = dataset.targets ? dataset.targets.join(", ") : "";
+      }
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < files.length; i++) {
+        files[i].assay_term_name = dataset.method;
+        files[i].biosample_ontology = dataset.biosample_ontology;
+        files[i].file_format = files[i].href.split(".")[1];
+        files[i].dataset = dataset.dataset_rel;
+        files[i].title = files[i].accession;
+        files[i].target = target;
+        files[i].biosample = dataset.biosample_ontology.term_name || "";
+        files[i].assay = dataset.method || "";
+        files[i].organ =
+          dataset.biosample_ontology.classification === "tissue"
+            ? dataset.biosample_ontology.organ_slims.join(", ")
+            : dataset.biosample_ontology.cell_slims.join(", ");
+      }
+      filesForGenomeBrowser = filesForGenomeBrowser.concat(
+        dataset.files_for_genome_browser
+      );
+    });
+
+    const numberOfPeaks =
+      chipData.length +
+      dnaseData.length +
+      histoneData.length +
+      QTLData.length +
+      motifsData.length;
+
+    return (
+      <>
+        <Breadcrumbs />
+        <RegulomeVersionTag />
+        <PagePreamble />
         <DataPanel>
           <DataArea>
             <DataItemLabel>Searched Coordinates</DataItemLabel>
-            <DataItemValue>{data.query_coordinates[0]}</DataItemValue>
+            <DataItemValue>{coordinates}</DataItemValue>
             <DataItemLabel>Genome Assembly</DataItemLabel>
             <DataItemValue>{data.assembly}</DataItemValue>
             <DataItemLabel>Rank</DataItemLabel>
             <DataItemValue>{data.regulome_score.ranking}</DataItemValue>
             <DataItemLabel>Score</DataItemLabel>
             <DataItemValue>{data.regulome_score.probability}</DataItemValue>
+            <DataItemLabel>Number of Total Peaks</DataItemLabel>
+            <DataItemValue>{numberOfPeaks}</DataItemValue>
+            <DataItemLabel>Number of ChIP-seq Peaks</DataItemLabel>
+            <DataItemValue>{chipData.length}</DataItemValue>
+            <DataItemLabel>Number of Accessibility Peaks</DataItemLabel>
+            <DataItemValue>{dnaseData.length}</DataItemValue>
+            <DataItemLabel>Number of Motifs Peaks</DataItemLabel>
+            <DataItemValue>{motifsData.length}</DataItemValue>
+            <DataItemLabel>Number of QTL Peaks</DataItemLabel>
+            <DataItemValue>{QTLData.length}</DataItemValue>
+            <DataItemLabel>Number of Histone ChIP-seq Peaks</DataItemLabel>
+            <DataItemValue>{histoneData.length}</DataItemValue>
+            <DataItemLabel>
+              Number of Files to Show in Genome Browser
+            </DataItemLabel>
+            <DataItemValue>{filesForGenomeBrowser.length}</DataItemValue>
+            <DataItemLabel>Number of Chromatin State Datasets</DataItemLabel>
+            <DataItemValue>{chromatinData.length}</DataItemValue>
+            {Object.keys(hitSnps).map((rsid) => (
+              <React.Fragment key={rsid}>
+                <DataItemLabel>{rsid}</DataItemLabel>
+                <DataItemValue>
+                  <div>
+                    {hitSnps[rsid].slice(0, 3).map((populationInfo) => (
+                      <div
+                        key={populationInfo.population}
+                      >{`${populationInfo.info} (${populationInfo.population})`}</div>
+                    ))}
+                  </div>
+                  {hitSnps[rsid].length > 3 && showMoreFreqs ? (
+                    <div>
+                      {hitSnps[rsid]
+                        .slice(3, hitSnps[rsid].length)
+                        .map((populationInfo) => (
+                          <div
+                            key={populationInfo.population}
+                          >{`${populationInfo.info} (${populationInfo.population})`}</div>
+                        ))}
+                    </div>
+                  ) : null}
+                  {hitSnps[rsid].length > defaultDisplayCount ? (
+                    <Button
+                      type="secondary"
+                      onClick={() => setShowMoreFreqs(!showMoreFreqs)}
+                    >
+                      {hitSnps[rsid].length - 3}{" "}
+                      {showMoreFreqs ? "fewer" : "more"}
+                    </Button>
+                  ) : null}
+                </DataItemValue>
+              </React.Fragment>
+            ))}
           </DataArea>
         </DataPanel>
-      )}
-      {Object.keys(data.notifications).length > 0 && (
-        <Notifications notifications={data.notifications} />
-      )}
+        {data.nearby_snps?.length > 0 ? <SnpsDiagram data={data} /> : null}
+        <div id="container"></div>
+      </>
+    );
+  }
+  return (
+    <>
+      <Breadcrumbs />
+      <RegulomeVersionTag />
+      <PagePreamble />
+      <Notifications notifications={data.notifications} />
     </>
   );
 }
