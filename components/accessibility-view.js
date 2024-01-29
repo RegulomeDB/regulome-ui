7476cb6029424c3bd80c69e085e3845eb2daa9d9
Mingjie Li <mingjiecn@gmail.com>
2024-01-29 09:43:42 -0600

    update accessibility view

--- components/accessibility-view.js
+++ components/accessibility-view.js
@@ -2,8 +2,17 @@ import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { XCircleIcon } from "@heroicons/react/20/solid";
 import { DataAreaTitle, DataPanel } from "./data-area";
 import AccessibilityDataTable from "./accessibility-table";
+import { BodyMapThumbnailAndModal } from "./body-map";
+import {
+  getFillColorHex,
+  getFillColorTailwind,
+  getFilteredData,
+  getOrganFacets,
+  getOrganFilter,
+} from "../lib/tissue-specific-score";
 
 // To dynamically load component ChipDataBarChart on the client side,
 // use the ssr option to disable server-rendering since AccessibilityChart relies on browser APIs like window.
@@ -12,16 +21,62 @@ const AccessibilityChart = dynamic(() => import("./accessibility-chart"), {
 });
 
 /**
+ *  Display selected filters and allow user to de-select them.
+ */
+function Selections({ filters, clearFilterFunc }) {
+  return (
+    <div>
+      <span>Selected filters:</span>
+      {filters.map((filter) => {
+        return (
+          <button
+            key={`${filter}-selected-filter`}
+            onClick={() => clearFilterFunc(filter)}
+            className="flex text-data-label text-sm"
+          >
+            <XCircleIcon className="h-5" />
+            {filter}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+Selections.propTypes = {
+  filters: PropTypes.array.isRequired,
+  clearFilterFunc: PropTypes.func.isRequired,
+};
+
+/**
  * This is the view for display accessibility data for a variant.
  */
-export function AccessibilityDataView({ data }) {
+export function AccessibilityDataView({
+  data,
+  normalizedTissueSpecificScore,
+  assembly,
+}) {
   const [showAccessibilityData, setShowAccessibilityData] = useState(false);
+  const [organFilters, setOrganFilters] = useState([]);
+
   const router = useRouter();
 
   useEffect(() => {
     const isChip = router.asPath.endsWith(`#!accessibility`);
     setShowAccessibilityData(isChip);
   }, [router]);
+
+  const filteredData = getFilteredData(data, organFilters);
+
+  function handleClickOrgan(organ, organList, enabledOrganList) {
+    const filters = getOrganFilter(
+      organFilters,
+      organ,
+      organList,
+      enabledOrganList
+    );
+    setOrganFilters(filters);
+  }
   return (
     showAccessibilityData && (
       <>
@@ -29,11 +84,37 @@ export function AccessibilityDataView({ data }) {
           <>
             <DataAreaTitle>Accessibility Data</DataAreaTitle>
             <DataPanel>
-              <AccessibilityChart accessibilityData={data}></AccessibilityChart>
+              <div className="@container">
+                <div className="grid @5xl:grid-cols-5  @5xl:grid-rows-1 grid-cols-1 grid-rows-2 gap-1 mb-2 justify-items-center">
+                  <div className="@5xl:col-span-1 w-56">
+                    <BodyMapThumbnailAndModal
+                      data={data}
+                      assembly={assembly}
+                      organFilters={organFilters}
+                      handleClickOrgan={handleClickOrgan}
+                      getOrganFacets={getOrganFacets}
+                      getFillColorTailwind={getFillColorTailwind}
+                      getFillColorHex={getFillColorHex}
+                      normalizedTissueSpecificScore={
+                        normalizedTissueSpecificScore
+                      }
+                    />
+                    {organFilters.length > 0 && (
+                      <Selections
+                        filters={organFilters}
+                        clearFilterFunc={handleClickOrgan}
+                      />
+                    )}
+                  </div>
+                  <div className="@5xl:col-span-4  w-11/12	">
+                    <AccessibilityChart accessibilityData={filteredData} />
+                  </div>
+                </div>
+              </div>
             </DataPanel>
             <DataAreaTitle>Datasets Table</DataAreaTitle>
             <DataPanel>
-              <AccessibilityDataTable data={data} />
+              <AccessibilityDataTable data={filteredData} />
             </DataPanel>
           </>
         ) : (
@@ -51,4 +132,6 @@ export function AccessibilityDataView({ data }) {
 
 AccessibilityDataView.propTypes = {
   data: PropTypes.array.isRequired,
+  normalizedTissueSpecificScore: PropTypes.object.isRequired,
+  assembly: PropTypes.string.isRequired,
 };
