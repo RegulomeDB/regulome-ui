2e4cf85152521cc052c89c23d3490f163ffe0fcf
Mingjie Li <mingjiecn@gmail.com>
2024-03-15 14:34:16 -0500

    filter variant ld table by ancestry

--- components/variant-ld-table.js
+++ components/variant-ld-table.js
@@ -1,8 +1,11 @@
 // node_modules
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 // components
 import SortableGrid from "./sortable-grid";
+import { sanitizedString } from "../lib/general";
+import { useState } from "react";
 
 const initialSort = {
   columnId: "score",
@@ -52,13 +55,34 @@ const variantLDColumns = [
 /**
  * Display a sortable table of the given data.
  */ export default function VariantLDTable({ data }) {
+  const [textInput, setTextInput] = useState("");
+  data =
+    textInput === ""
+      ? data
+      : data.filter((dataset) =>
+          sanitizedString(dataset.ancestry).includes(sanitizedString(textInput))
+        );
   return (
-    <SortableGrid
-      data={data}
-      columns={variantLDColumns}
-      initialSort={initialSort}
-      pager={{}}
-    />
+    <div className="grid gap-y-2">
+      <label className="relative text-gray-400 focus-within:text-gray-600 block">
+        <MagnifyingGlassIcon className="absolute top-1/3 left-1 w-6 h-4" />
+        <input
+          className="bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-7 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-brand"
+          type="search"
+          aria-label="search by filtering ancestry"
+          placeholder="Search for the ancestry"
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+          name="ancestry"
+        />
+      </label>
+      <SortableGrid
+        data={data}
+        columns={variantLDColumns}
+        initialSort={initialSort}
+        pager={{}}
+      />
+    </div>
   );
 }
 
