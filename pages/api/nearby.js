a59d3477defc0e440f6e86e3ce9ccab16ca2d1b2
Mingjie Li <mingjiecn@gmail.com>
2024-03-29 15:54:35 -0500

    update limit and add docmentation

--- pages/api/nearby.js
+++ pages/api/nearby.js
@@ -2,9 +2,9 @@ import fetchNearby from "../../lib/fetch-nearby";
 import FetchRequest from "../../lib/fetch-request";
 
 /**
- * This api is used for querying variants in variant LD data and score them for any given single variant.
- * @returns {arry} data in json format. The data returned will include
- * a list of variants with their score info.
+ * This api is used for querying nearest genes and regulatory regions for any given single variant.
+ * @returns {object} data in json format. The data returned will include
+ * the coordinates of the display region, the nearest genes and the regulatory regions in the display region.
  */
 export default async function handler(req, res) {
   const request = new FetchRequest();
