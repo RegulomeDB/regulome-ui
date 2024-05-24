7ee68b8ad29816c92e495d656ab15ff61361d49c
Mingjie Li <mingjiecn@gmail.com>
2024-05-24 09:55:44 -0500

    use gtex color for plots

--- components/accessibility-chart.js
+++ components/accessibility-chart.js
@@ -18,6 +18,7 @@ import {
   getBarChartOptions,
   getBarChartThumbnailOptions,
 } from "../lib/chart-options";
+import { GtexColor } from "../lib/tissue-specific-score";
 
 ChartJS.register(
   CategoryScale,
@@ -49,22 +50,43 @@ export default function AccessibilityChart({
   const counts = accessibilityData.reduce((groupCountByBiosample, dataset) => {
     const biosample = dataset.biosample_ontology.term_name;
     if (biosample in groupCountByBiosample) {
-      groupCountByBiosample[biosample] += 1;
+      groupCountByBiosample[biosample].count += 1;
     } else {
-      groupCountByBiosample[biosample] = 1;
+      groupCountByBiosample[biosample] = {};
+      groupCountByBiosample[biosample].count = 1;
+      const organs = dataset.biosample_ontology.organ_slims;
+      organs.sort((a, b) => {
+        const scoreA = GtexColor[a] ? GtexColor[a].priority : 99;
+        const scoreB = GtexColor[b] ? GtexColor[b].priority : 99;
+        return scoreA - scoreB;
+      });
+      groupCountByBiosample[biosample].color =
+        organs[0] && GtexColor[organs[0]]
+          ? GtexColor[organs[0]].hex
+          : "#808080";
+      const organsScore = organs.map((organ) =>
+        GtexColor[organ] ? GtexColor[organ].priority : 99
+      );
+      if (organs.length >= 2 && organsScore[0] === organsScore[1]) {
+        console.log(organs.join(", "), organsScore.join(", "));
+      }
     }
     return groupCountByBiosample;
   }, {});
   // biosamples are sorted by its group count
   let biosamples = Object.keys(counts).sort((a, b) => {
-    return counts[b] - counts[a];
+    return counts[b].count - counts[a].count;
   });
   let groupCounts = biosamples.map((label) => {
-    return counts[label];
+    return counts[label].count;
+  });
+  let organColors = biosamples.map((label) => {
+    return counts[label].color;
   });
   if (thumbnail && groupCounts.length > 10) {
     groupCounts = groupCounts.slice(0, 10);
     biosamples = biosamples.slice(0, 10);
+    organColors = organColors.slice(0, 10);
   }
   const data = {
     labels: biosamples,
@@ -72,7 +94,7 @@ export default function AccessibilityChart({
       {
         label: "Number of accessibility datasets",
         data: groupCounts,
-        backgroundColor: colors.cyan[700],
+        backgroundColor: organColors,
         maxBarThickness: 50,
       },
     ],
--- components/qtl-chart.js
+++ components/qtl-chart.js
@@ -18,6 +18,7 @@ import {
   getBarChartOptions,
   getBarChartThumbnailOptions,
 } from "../lib/chart-options";
+import { GtexColor } from "../lib/tissue-specific-score";
 
 ChartJS.register(
   CategoryScale,
@@ -45,22 +46,42 @@ export default function QTLChart({ qtlData, height = 600, thumbnail }) {
   const counts = qtlData.reduce((groupCountByBiosample, dataset) => {
     const biosample = dataset.biosample_ontology.term_name;
     if (biosample in groupCountByBiosample) {
-      groupCountByBiosample[biosample] += 1;
+      groupCountByBiosample[biosample].count += 1;
     } else {
-      groupCountByBiosample[biosample] = 1;
+      groupCountByBiosample[biosample] = {};
+      groupCountByBiosample[biosample].count = 1;
+      const organs = dataset.biosample_ontology.organ_slims;
+      organs.sort((a, b) => {
+        const scoreA = GtexColor[a] ? GtexColor[a].priority : 99;
+        const scoreB = GtexColor[b] ? GtexColor[b].priority : 99;
+        return scoreA - scoreB;
+      });
+      groupCountByBiosample[biosample].color =
+        organs[0] && GtexColor[organs[0]]
+          ? GtexColor[organs[0]].hex
+          : "#808080";
+      const organsScore = organs.map((organ) =>
+        GtexColor[organ] ? GtexColor[organ].priority : 99
+      );
+      if (organs.length >= 2 && organsScore[0] === organsScore[1]) {
+        console.log(organs.join(", "), organsScore.join(", "));
+      }
     }
     return groupCountByBiosample;
   }, {});
-  // biosamples are sorted by its group count
   let biosamples = Object.keys(counts).sort((a, b) => {
-    return counts[b] - counts[a];
+    return counts[b].count - counts[a].count;
   });
   let groupCounts = biosamples.map((label) => {
-    return counts[label];
+    return counts[label].count;
+  });
+  let organColors = biosamples.map((label) => {
+    return counts[label].color;
   });
   if (thumbnail && groupCounts.length > 10) {
     groupCounts = groupCounts.slice(0, 10);
     biosamples = biosamples.slice(0, 10);
+    organColors = organColors.slice(0, 10);
   }
   const data = {
     labels: biosamples,
@@ -68,7 +89,7 @@ export default function QTLChart({ qtlData, height = 600, thumbnail }) {
       {
         label: "Number of QTL datasets",
         data: groupCounts,
-        backgroundColor: colors.cyan[700],
+        backgroundColor: organColors,
         maxBarThickness: 50,
       },
     ],
