dc4602595a4b2c88bf0a079f190ae95555ea5656
Otto Jolanki <ojolanki@stanford.edu>
2026-06-02 11:55:09 -0700

    fix tests

--- cdk/infrastructure/constructs/pipeline.py
+++ cdk/infrastructure/constructs/pipeline.py
@@ -126,7 +126,7 @@ class BasicSelfUpdatingPipeline(Construct):
             ],
             docker_enabled_for_synth=True,
             code_pipeline=self.underlying_pipeline,
-            cli_version=self.props.common.aws_cdk_version,
+            cli_version=self.props.config.common.aws_cdk_version,
         )
 
     def _get_underlying_pipeline(self) -> Pipeline:
