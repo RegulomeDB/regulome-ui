64b6b7c302ce89dfd7c8fea361318e02aa898728
Otto Jolanki <ojolanki@stanford.edu>
2026-06-02 12:33:23 -0700

    replace backend url

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -52,7 +52,7 @@ config: Dict[str, Any] = {
                 'desired_count': 1,
                 'max_capacity': 4,
             },
-            'backend_url': 'https://gds-for-regulome-demo.demo.regulomedb.org',
+            'backend_url': 'https://regulome-es-2-2-rc.demo.regulomedb.org',
             'tags': [
                 ('time-to-live-hours', '72'),
                 ('turn-off-on-friday-night', 'yes'),
